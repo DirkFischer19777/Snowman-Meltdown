96febd4e62ff9789247fbf4d23b39d6f8fac3cf3
dirkfischer <fischertechnic@gmx.net>
2025-08-12 16:17:39 +0200

    - creaated gamelogic.py and implemented game logic - created ascii_art.py and implemented stages -implemented repeat game function in snowman.py - input validation

--- snowman.py
+++ snowman.py
@@ -1,85 +1,18 @@
-import random
-
-# Snowman ASCII Art stages
-STAGES = [
-     # Stage 0: Full snowman
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     ( : ) 
-     """,
-     # Stage 1: Bottom part starts melting
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     """,
-     # Stage 2: Only the head remains
-     """
-      ___  
-     /___\\ 
-     (o o) 
-     """,
-     # Stage 3: Snowman completely melted
-     """
-      ___  
-     /___\\ 
-     """
- ]
-
-# List of secret words
-WORDS = ["python", "git", "github", "snowman", "meltdown"]
+import game_logic
 
 
-def get_random_word():
-    """Selects a random word from the list."""
-    return WORDS[random.randint(0, len(WORDS) - 1)]
 
 
-def play_game():
-    secret_word = get_random_word()
-    guessed_letters = []
-    mistakes = 0
-    print("Welcome to Snowman Meltdown!")
-    print("Secret word selected: " + secret_word)  # for testing, later remove this line
 
-
+def repeat_game():
     while True:
-        # For now, simply prompt the user once:
-        word_status= display_game_state(mistakes, secret_word, guessed_letters)
-        compare_lst = word_status.split()
-        compare_wrd = "".join(compare_lst)
-        if compare_wrd == secret_word:
-            print("you solved the word" )
+        game_logic.play_game()
+        again = input("Do you want to play again? (y/n): ").lower()
+        if again != "y":
+            print("Thanks for playing! Goodbye.")
             break
-        guess = input("Guess a letter: ").lower()
-        print("You guessed:", guess)
-        guessed_letters.append(guess)
 
-        if guess not in secret_word:
-            mistakes +=1
-            if mistakes > len(STAGES) - 1:
-                print("you have failed")
-                break
-
-
-def display_game_state(mistakes, secret_word, guessed_letters):
-    # Display the snowman stage for the current number of mistakes.
-    print(STAGES[mistakes])
-    # Build a display version of the secret word.
-    display_word = ""
-    for letter in secret_word:
-        if letter in guessed_letters:
-            display_word += letter + " "
-        else:
-            display_word += "_ "
-    print("Word: ", display_word)
-    print("\n")
-    return display_word
 
 
 if __name__ == "__main__":
-    play_game()
+    repeat_game()
